ddc006f647902d9ea451cfb30c2f1003edb08821
matovu-farid <matovu90@gmail.com>
2022-01-31 17:12:00 +0300

    Test webpack configuration

---
+++ dist/bundle.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+var func = function func() {
+  console.log('Hello world');
+};
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUEsSUFBTUEsSUFBSSxHQUFHLFNBQVBBLElBQU8sR0FBSTtBQUNiQyxFQUFBQSxPQUFPLENBQUNDLEdBQVIsQ0FBWSxhQUFaO0FBQ0gsQ0FGRCxDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vcG9rZW1vbi8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zdCBmdW5jID0gKCk9PntcclxuICAgIGNvbnNvbGUubG9nKCdIZWxsbyB3b3JsZCcpXHJcbn0iXSwibmFtZXMiOlsiZnVuYyIsImNvbnNvbGUiLCJsb2ciXSwic291cmNlUm9vdCI6IiJ9
