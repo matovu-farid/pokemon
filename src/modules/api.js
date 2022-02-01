48e75168dda53a99e2eb5c48b9cf0429c5a325de
jihaneh <jijih@hotmail.fr>
2022-02-01 12:43:51 +0100

    Update API : create external constante for the URL

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,24 +1,26 @@
 export default class Api {
-    #fetchResults= async () => {
-      const { results } = await (await fetch('https://pokeapi.co/api/v2/pokemon?limit=6&offset=10')).json();
-      return results;
-    }
+  POKEMON_URL = 'https://pokeapi.co/api/v2/pokemon';
 
-    #fetchPockemon=async (url) => {
-      const result = {};
-      const data = await (await fetch(url)).json();
-      result.url = data.sprites.other.dream_world.front_default;
-      result.id = data.id;
-      return result;
-    }
+  #fetchResults = async () => {
+    const { results } = await (await fetch(`${this.POKEMON_URL}?limit=6&offset=10`)).json();
+    return results;
+  };
 
-    getDisplayItems = async () => {
-      const promises = [];
-      const results = await this.#fetchResults();
-      results.forEach((result) => {
-        promises.push(this.#fetchPockemon(result.url));
-      });
-      const items = await Promise.all(promises);
-      return items;
-    }
+  #fetchPockemon = async (url) => {
+    const result = {};
+    const data = await (await fetch(url)).json();
+    result.url = data.sprites.other.dream_world.front_default;
+    result.id = data.id;
+    return result;
+  };
+
+  getDisplayItems = async () => {
+    const promises = [];
+    const results = await this.#fetchResults();
+    results.forEach((result) => {
+      promises.push(this.#fetchPockemon(result.url));
+    });
+    const items = await Promise.all(promises);
+    return items;
+  };
 }
