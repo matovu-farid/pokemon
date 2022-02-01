4dab30f55e9c67c299cdf422ad7bd86f31f16cf2
jihaneh <jijih@hotmail.fr>
2022-02-01 13:56:58 +0100

    Update project to load comments for a specific pokemon

--- src/modules/popup.js
+++ src/modules/popup.js
@@ -1,14 +1,19 @@
 import { selector, selectorAll } from './tools.js';
+import { appendCommentItem } from './create_listcomment';
 
 export const popupInit = (connector) => {
   selectorAll('button[id^="comment-"], .popup-close').forEach((element) => {
     element.addEventListener('click', async (event) => {
       if (selector('.popup-modal').classList.contains('popup-hidden')) {
-        const pokemon = await connector.getPokemonFromId(event.target.id.match(/\d+/g)[0]);
+        const idPokemon = event.target.id.match(/\d+/g)[0];
+        const pokemon = await connector.getPokemonFromId(idPokemon);
         selector('#pokemon_name').innerHTML = pokemon.name;
         selector('#pokemon_image').src = pokemon.url;
         selector('#pokemon_height').innerHTML = pokemon.height;
         selector('#pokemon_weight').innerHTML = pokemon.weight;
+
+        const comments = await connector.getComments(idPokemon);
+        comments.forEach(appendCommentItem);
       }
       selector('.popup-modal').classList.toggle('popup-hidden');
     });
