1021442d9b70a446b37a5c450031e8c687f4aa81
matovu-farid <matovu90@gmail.com>
2022-02-02 02:58:20 +0300

    Fix: eslint errors

--- test/item_counter.test.js
+++ test/item_counter.test.js
@@ -1,14 +1,13 @@
 import {
-  jest, expect, it, beforeEach,describe
+  jest, expect, it, beforeEach, describe,
 } from '@jest/globals';
 import Api from '../src/modules/api.js';
 import 'regenerator-runtime/runtime';
 import { displayItems } from '../src/modules/display_list_items';
 
-
 jest.mock('../src/modules/api.js');
 
-describe('The counter',()=>{
+describe('The counter', () => {
   document.body.innerHTML = `
   <span id="count"></span>
   <section class="centered-column">
@@ -25,35 +24,32 @@ describe('The counter',()=>{
       </section>
   `;
   });
-  
 
+  it('should mock the api', () => {
+    const api = new Api();
 
-it('should mock the api', () => {
-  const api = new Api();
+    expect(api.getDisplayItems()).resolves.toEqual([
+      {
+        url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/11.svg',
+        id: 11,
+      },
+      {
+        url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/12.svg',
+        id: 12,
+      },
+      {
+        url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/13.svg',
+        id: 13,
+      },
 
-  expect(api.getDisplayItems()).resolves.toEqual([
-    {
-      url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/11.svg',
-      id: 11,
-    },
-    {
-      url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/12.svg',
-      id: 12,
-    },
-    {
-      url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/13.svg',
-      id: 13,
-    },
+    ]);
+  });
 
-  ]);
+  it('should the value of (3) when displayItems is called', async () => {
+    await displayItems();
+    const displayList = document.querySelector('#display-items');
+    expect(displayList.children.length).toBe(3);
+    const count = document.querySelector('#count');
+    expect(count.innerHTML).toBe('(3)');
+  });
 });
-
-it('should the value of (3) when displayItems is called', async () => {
-  await displayItems();
-  const displayList = document.querySelector('#display-items');
-  expect(displayList.children.length).toBe(3);
-  const count = document.querySelector('#count');
-  expect(count.innerHTML).toBe('(3)');
-});
-
-})
