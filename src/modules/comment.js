6de92911a34fc8e2639e8b5f60d8e2bbfdc776da
jihaneh <jijih@hotmail.fr>
2022-02-02 11:12:52 +0100

    Add refresh popup with new commit

--- src/modules/comment.js
+++ src/modules/comment.js
@@ -1,5 +1,6 @@
 import { selector } from './tools.js';
 import Api from './api';
+import { appendCommentItem } from './create_listcomment';
 
 const form = selector('form');
 
@@ -7,7 +8,13 @@ selector('form').addEventListener('submit', async (event) => {
   event.preventDefault();
   const api = new Api();
   await api.addComment(form.idPokemon.value, form.username.value, form.comment.value);
+
+  let today = new Date();
+
+  appendCommentItem({
+    creation_date: `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(2, '0')}-${String(today.getDate()).padStart(2, '0')}`,
+    username: form.username.value,
+    comment: form.comment.value,
+  });
   form.reset();
-  selector('.pokemon-comments').innerHTML = '';
-  selector('.popup-modal').classList.add('popup-hidden');
 });
