83cc896dff7c7a7aefa50e8d0b217b842de12b10
jihaneh <jijih@hotmail.fr>
2022-02-02 21:47:44 +0100

    Add test for comment counter

--- src/modules/__mocks__/api.js
+++ src/modules/__mocks__/api.js
@@ -1,32 +1,66 @@
+import fetch from 'cross-fetch';
+
 export default class Api {
-    #fetchResults= async () => {
-      const { results } = await (await fetch('https://pokeapi.co/api/v2/pokemon?limit=6&offset=10')).json();
-      return results;
-    }
+  #fetchResults = async () => {
+    const { results } = await (await fetch('https://pokeapi.co/api/v2/pokemon?limit=6&offset=10')).json();
+    return results;
+  };
 
-    #fetchPockemon=async (url) => {
-      const result = {};
-      const data = await (await fetch(url)).json();
-      result.url = data.sprites.other.dream_world.front_default;
-      result.id = data.id;
-      return result;
-    }
+  #fetchPockemon = async (url) => {
+    const result = {};
+    const data = await (await fetch(url)).json();
+    result.url = data.sprites.other.dream_world.front_default;
+    result.id = data.id;
+    return result;
+  };
 
-    getDisplayItems = () => Promise.resolve(
-      [
-        {
-          url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/11.svg',
-          id: 11,
-        },
-        {
-          url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/12.svg',
-          id: 12,
-        },
-        {
-          url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/13.svg',
-          id: 13,
-        },
+  getDisplayItems = () => Promise.resolve([
+    {
+      url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/11.svg',
+      id: 11,
+    },
+    {
+      url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/12.svg',
+      id: 12,
+    },
+    {
+      url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/13.svg',
+      id: 13,
+    },
+  ]);
 
-      ],
-    )
+  getLikes = () => [
+    { likes: 9, item_id: '11' },
+    { item_id: 'bulbasaur', likes: 1 },
+    { likes: 9, item_id: '2' },
+  ];
+
+  // eslint-disable-next-line no-unused-vars
+  getComments = (idPokemon) => Promise.resolve([
+    {
+      comment: 'This is my favorite pokemon.',
+      username: 'Noemi',
+      creation_date: '2022-02-02',
+    },
+  ]);
+
+  // eslint-disable-next-line no-unused-vars
+  getPokemonFromId = async (idPokemon) => ({
+    base_experience: 64,
+    forms: [{ name: 'bulbasaur', url: 'https://pokeapi.co/api/v2/pokemon-form/1/' }],
+    height: 7,
+    held_items: [],
+    id: 1,
+    is_default: true,
+    location_area_encounters: 'https://pokeapi.co/api/v2/pokemon/1/encounters',
+    name: 'bulbasaur',
+    order: 1,
+    past_types: [],
+    species: { name: 'bulbasaur', url: 'https://pokeapi.co/api/v2/pokemon-species/1/' },
+    types: [
+      { slot: 1, type: { name: 'grass', url: 'https://pokeapi.co/api/v2/type/12/' } },
+      { slot: 2, type: { name: 'poison', url: 'https://pokeapi.co/api/v2/type/4/' } },
+    ],
+    weight: 69,
+  });
 }
---
+++ test/comment_counter.test.js
@@ -0,0 +1,69 @@
+import {
+  jest, expect, it, beforeEach, describe,
+} from '@jest/globals';
+import 'regenerator-runtime/runtime';
+import Api from '../src/modules/api.js';
+import { displayItems } from '../src/modules/display_list_items';
+
+jest.mock('../src/modules/api.js');
+
+describe('The comment counter', () => {
+  const dom = `
+  <span id="count"></span>
+  <section class="centered-column">
+          <ul id="display-items">
+          </ul>
+      </section>
+
+        <div class="popup-modal popup-hidden">
+        <div class="popup-datas">
+          <div class="popup-close">
+            <img src="close_popup.png" class="close_popup" alt="close popup" />
+          </div>
+          <div class="popup-image">
+            <img class="pokemon-image" id="pokemon_image" src="#" alt="pokemon picture" />
+          </div>
+          <div class="popup-pokemon">
+            <h3 id="pokemon_name">pokemon_name</h3>
+            <ul class="pokemon-characteristics">
+              <li>Height : <span id="pokemon_height"></span></li>
+              <li>Weight : <span id="pokemon_weight"></span></li>
+            </ul>
+            <h3>Comments <span id="nb-comments"></span></h3>
+            <ul class="pokemon-comments"></ul>
+            <h3>Add a comment</h3>
+            <form id="add-comment">
+              <input type="hidden" name="idPokemon" id="idPokemon" />
+              <input name="username" placeholder="Your name" />
+              <textarea name="comment" cols="30" rows="10" placeholder="Your insights"></textarea>
+              <button type="submit">Comment</button>
+            </form>
+          </div>
+        </div>
+      </div>
+  `;
+
+  document.body.innerHTML = dom;
+  beforeEach(() => {
+    document.body.innerHTML = dom;
+  });
+
+  it('should mock the api', async () => {
+    const api = new Api();
+    const comments = await api.getComments(5);
+    expect(comments).toEqual([
+      {
+        comment: 'This is my favorite pokemon.',
+        username: 'Noemi',
+        creation_date: '2022-02-02',
+      },
+    ]);
+  });
+
+  it('should the value of (1) when displayItems is called', async () => {
+    await displayItems();
+    await await document.querySelector('button[id^="comment-"]').click();
+    expect(document.querySelector('#nb-comments').innerHTML).toEqual('(1)');
+    expect(document.querySelector('.pokemon-comments').children.length).toEqual(1);
+  });
+});
