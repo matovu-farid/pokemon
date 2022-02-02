db3d8613ec9f291858953619929830a6bcda963a
matovu-farid <matovu90@gmail.com>
2022-02-02 23:04:50 +0300

    Styled the homepage

--- src/modules/create_listhtml.js
+++ src/modules/create_listhtml.js
@@ -1,7 +1,7 @@
 export const listHtml = (result) => `
     <img src="${result.url}" alt="image">
      <button id="comment-${result.id}">Comments</button>
-     <p id="name-${result.id}">${result.name} 
+     <p class="pokemon-name" id="name-${result.id}">${result.name} 
      <i id="off-${result.id}" class="fa fa-heart-o heart" aria-hidden="true"></i></p> 
      <p class="like" id="likes-${result.id}"></p>
     `;
@@ -9,7 +9,7 @@ export const listHtml = (result) => `
 export const appendListItem = (result) => {
   const listItem = document.createElement('li');
   listItem.innerHTML = listHtml(result);
-  listItem.className = 'centered-column';
+  listItem.classList.add(...['centered-column','card']);
   const displayItems = document.querySelector('#display-items');
   displayItems.append(listItem);
 };
