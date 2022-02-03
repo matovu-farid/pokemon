51c50b9426a538ca1f805c2072097a4574225fd2
matovu-farid <matovu90@gmail.com>
2022-02-03 14:03:23 +0300

    Positioned the load more and show less buttons

--- src/modules/create_listhtml.js
+++ src/modules/create_listhtml.js
@@ -1,9 +1,9 @@
 export const listHtml = (result) => `
     <img src="${result.url}" alt="image">
-     <button id="comment-${result.id}">Comments</button>
-     <p class="pokemon-name" id="name-${result.id}">${result.name} 
-     <i id="off-${result.id}" class="fa fa-heart-o heart" aria-hidden="true"></i></p> 
-     <p class="like" id="likes-${result.id}"></p>
+    <p class="pokemon-name" id="name-${result.id}">${result.name} 
+    <i id="off-${result.id}" class="fa fa-heart-o heart" aria-hidden="true"></i></p> 
+    <p class="like" id="likes-${result.id}"></p>
+    <button id="comment-${result.id}">Comments</button>
     `;
 
 export const appendListItem = (result) => {
