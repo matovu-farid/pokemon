f092958cd9bae469c6aa1041e37824f64e4b10c4
jihaneh <jijih@hotmail.fr>
2022-02-04 13:25:12 +0100

    Add comment button on homepage

--- src/modules/create_listhtml.js
+++ src/modules/create_listhtml.js
@@ -1,12 +1,13 @@
 export const listHtml = (result) => `
     <p class="pokemon-name" id="name-${result.id}">${result.name}</p> 
     <img src="${result.url}" alt="image"> 
+    <button id="comment-${result.id}">Comments</button>
     `;
 /*
 
     <i id="off-${result.id}" class="fa fa-heart-o heart" aria-hidden="true"></i></p>
     <p class="like" id="likes-${result.id}"></p>
-    <button id="comment-${result.id}">Comments</button>
+
     */
 export const appendListItem = (result) => {
   const listItem = document.createElement('li');
