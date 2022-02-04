c2f6340352f946ec7f5d94eb45ee95787795dc52
jihaneh <jijih@hotmail.fr>
2022-02-04 15:50:37 +0100

    Activate like

--- src/modules/create_listhtml.js
+++ src/modules/create_listhtml.js
@@ -1,14 +1,11 @@
 export const listHtml = (result) => `
     <p class="pokemon-name" id="name-${result.id}">${result.name}</p> 
     <img src="${result.url}" alt="image"> 
+    <p class="pokemon-likes"> <span class="like" id="likes-${result.id}"></span>
+    <i id="off-${result.id}" class="fa fa-heart-o heart" aria-hidden="true"></i> </p>
     <button id="comment-${result.id}">Comments</button>
     `;
-/*
 
-    <i id="off-${result.id}" class="fa fa-heart-o heart" aria-hidden="true"></i></p>
-    <p class="like" id="likes-${result.id}"></p>
-
-    */
 export const appendListItem = (result) => {
   const listItem = document.createElement('li');
   listItem.innerHTML = listHtml(result);
