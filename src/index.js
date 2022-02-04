49d03b0969ce2a9f121046297acf27d34e3ecd96
jihaneh <jijih@hotmail.fr>
2022-02-04 12:56:00 +0100

    Update link to App

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import './style.css';
-import './modules/popup.js';
-import './modules/display_list_items';
-import './modules/like_listener';
-import './modules/comment';
-import './modules/load_pokemon';
+
+import { displayItems } from './modules/app';
+
+displayItems();
