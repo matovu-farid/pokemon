d0acccf3b863d880410cb75b08e8691a94eee4a5
jihaneh <jijih@hotmail.fr>
2022-02-04 12:57:16 +0100

    Comment temporary non use code

--- src/modules/create_listhtml.js
+++ src/modules/create_listhtml.js
@@ -1,11 +1,13 @@
 export const listHtml = (result) => `
-    <img src="${result.url}" alt="image">
-    <p class="pokemon-name" id="name-${result.id}">${result.name} 
-    <i id="off-${result.id}" class="fa fa-heart-o heart" aria-hidden="true"></i></p> 
+    <p class="pokemon-name" id="name-${result.id}">${result.name}</p> 
+    <img src="${result.url}" alt="image"> 
+    `;
+/*
+
+    <i id="off-${result.id}" class="fa fa-heart-o heart" aria-hidden="true"></i></p>
     <p class="like" id="likes-${result.id}"></p>
     <button id="comment-${result.id}">Comments</button>
-    `;
-
+    */
 export const appendListItem = (result) => {
   const listItem = document.createElement('li');
   listItem.innerHTML = listHtml(result);
