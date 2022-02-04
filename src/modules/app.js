95d71a59a20f9851da1c36862240f0c0a369e10e
jihaneh <jijih@hotmail.fr>
2022-02-04 13:36:21 +0100

    Active popup

--- src/modules/app.js
+++ src/modules/app.js
@@ -1,28 +1,23 @@
 import api from './api';
 import { appendListItem } from './create_listhtml';
-
-/* import './like_listener';
-import './load_pokemon';
 import './like_listener';
-import './load_pokemon';
+//import './load_pokemon';
 
 import likeListener from './like_listener';
 import appendNoOFLikes from './append_no_of_likes';
-import { appendCount } from './counter';
+//import { appendCount } from './counter';
 import { popupInit } from './popup';
-*/
+
 export const displayItems = async () => {
   const displayItems = await api.getDisplayItems();
   displayItems.forEach(appendListItem);
-  /* appendCount();
+  //appendCount();
   popupInit(api);
   likeListener();
   const likes = await api.getLikes();
   appendNoOFLikes(likes);
-  */
 };
 
-/*
 export const displayLess = async () => {
   if (api.POCKEMON_COUNT > 6) {
     api.POCKEMON_COUNT -= 6;
@@ -34,6 +29,5 @@ export const displayLess = async () => {
     appendCount();
   }
 };
-*/
 
 export default displayItems;
